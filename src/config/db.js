9e1f3a7bea55ae048bf0bf4cac54342d8b8be29a
Stekz42 <159025592+Stekz42@users.noreply.github.com>
2025-06-03 15:33:31 +0200

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,23 +3,11 @@ const { MongoClient } = require('mongodb');
 const MONGODB_URI = process.env.MONGODB_URI;
 const DB_NAME = 'geojson';
 
-let client = null;
-
 async function connectToDatabase() {
-  if (!client) {
-    client = new MongoClient(MONGODB_URI);
-    await client.connect();
-    console.log('Verbunden mit MongoDB');
-  }
-  return client.db(DB_NAME);
+  const client = new MongoClient(MONGODB_URI);
+  await client.connect();
+  console.log('Verbunden mit MongoDB');
+  return { db: client.db(DB_NAME), client };
 }
 
-async function closeDatabase() {
-  if (client) {
-    await client.close();
-    client = null;
-    console.log('MongoDB-Verbindung geschlossen');
-  }
-}
-
-module.exports = { connectToDatabase, closeDatabase };
+module.exports = { connectToDatabase };
